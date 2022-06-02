49f6f424b6de64bd7f7618b2d7f80f16b6f85979
Leks3472 <ras.ufa@mail.ru>
2022-06-02 21:01:49 +0500

    2.1.3

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,13 +1,22 @@
+//https://www.youtube.com/watch?v=pDTzRsuGrDU
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnimalsCage {
 
     @Autowired
+    @Qualifier("dog")
     private Animal animal;
+
+
+    private Timer timer;
+    public Timer getTimer() {
+        return timer;
+    }
 
     public void whatAnimalSay() {
         System.out.println("Say:");
